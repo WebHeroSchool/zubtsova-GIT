fed9982ea1832a440e9973778bb35893497dd4a6
Viktoriya <>
2021-04-07 15:03:15 +0700

    no message

--- src/components/About/About.js
+++ src/components/About/About.js
@@ -22,19 +22,19 @@ class About extends React.Component {
     octokit.users.getByUsername({
       username: 'viktoriya-zubtsova'
     }).then(( json ) => {
-        this.setState({
-          userName: json.data.login,
-          userAvatar: json.data.avatar_url,
-          userUrl: json.data.html_url,
-          userBio: json.data.bio
+      this.setState({
+        userName: json.data.login,
+        userAvatar: json.data.avatar_url,
+        userUrl: json.data.html_url,
+        userBio: json.data.bio
       });
     }).catch(error => {
       this.setState({
         error: true,
         isLoading: false,
         textError: 'Что-то пошло не так...'
-    })
-  });
+      })
+    });
     octokit.repos.listForUser({
       username: 'viktoriya-zubtsova'
     }).then(({ data }) => {
@@ -46,77 +46,77 @@ class About extends React.Component {
         repoPaginList: this.state.repoList.slice(0, this.state.paginLimit),
         paginCount: Math.ceil(this.state.repoList.length / this.state.paginLimit)
       });
-      }).catch(error => {
-        this.setState({
-          error: true,
-          isLoading: false,
-          textError: 'Что-то пошло не так...'
+    }).catch(error => {
+      this.setState({
+        error: true,
+        isLoading: false,
+        textError: 'Что-то пошло не так...'
       })
     });
   }
   changePagin(event, value) {
-        this.setState({
-          currentPage: value,
-          repoPaginList: this.state.repoList.slice((value - 1) * this.state.paginLimit, ((value - 1) * this.state.paginLimit + this.state.paginLimit))
-        });
-}
+    this.setState({
+      currentPage: value,
+      repoPaginList: this.state.repoList.slice((value - 1) * this.state.paginLimit, ((value - 1) * this.state.paginLimit + this.state.paginLimit))
+    });
+  }
 
   render() {
     const { isLoading, repoPaginList, error, paginCount} = this.state;
 
     if (this.state.error) {
-			return (<div>
-				<h2 className={styles.title}>{this.state.textError}</h2>
-			</div>);
-		} else {
+     return (<div>
+      <h2 className={styles.title}>{this.state.textError}</h2>
+      </div>);
+   } else {
     return (
       <div>
-        {!isLoading && <div>
-          <div className={styles.user}>
-            <img className={styles.userAvatar} src={this.state.userAvatar}/>
-            <div className={styles.userInfo}>
-              <h3 className={styles.userTitle}>Виктория Зубцовa</h3>
-              <p className={styles.userText}>{this.state.userBio}</p>
-              <p className={styles.userText}>
-                <MailOutlineIcon className={styles.userIcon} /> wow55222@yandex.ru</p>
-              <p className={styles.userText}>
-                <TelegramIcon className={styles.userIcon} /><WhatsAppIcon className={styles.userIcon} /> +79130822502</p>
-              <p className={styles.userText}>
-                <InstagramIcon className={styles.userIcon} />
-                <a className={styles.link} href={'https://www.instagram.com/zy_vi_an'}> zy_vi_an</a></p>
-              <p className={styles.userText}>
-                <GitHubIcon className={styles.userIcon} />
-                <a className={styles.link} href={this.state.userUrl}> {this.state.userName}</a></p>
-            </div>
-          </div>
-          <h2 className={styles.title}>Мои проекты:</h2>
-          <ul>
-            <li><a className={styles.projectLink} href='https://webheroschool.github.io/zubtsova-final-HTML-CSS/'>Axion</a></li>
-            <li><a className={styles.projectLink} href='https://viktoriya-zubtsova.github.io/Bali/'>Bali</a></li>
-            <li><a className={styles.projectLink} href='https://webheroschool.github.io/zubtsova-JS/'>Карты с багами оО</a></li>
-          </ul>
+      {!isLoading && <div>
+        <div className={styles.user}>
+        <img className={styles.userAvatar} src={this.state.userAvatar}/>
+        <div className={styles.userInfo}>
+        <h3 className={styles.userTitle}>Виктория Зубцовa</h3>
+        <p className={styles.userText}>{this.state.userBio}</p>
+        <p className={styles.userText}>
+        <MailOutlineIcon className={styles.userIcon} /> wow55222@yandex.ru</p>
+        <p className={styles.userText}>
+        <TelegramIcon className={styles.userIcon} /><WhatsAppIcon className={styles.userIcon} /> +79130822502</p>
+        <p className={styles.userText}>
+        <InstagramIcon className={styles.userIcon} />
+        <a className={styles.link} href={'https://www.instagram.com/zy_vi_an'}> zy_vi_an</a></p>
+        <p className={styles.userText}>
+        <GitHubIcon className={styles.userIcon} />
+        <a className={styles.link} href={this.state.userUrl}> {this.state.userName}</a></p>
+        </div>
+        </div>
+        <h2 className={styles.title}>Мои проекты:</h2>
+        <ul>
+        <li><a className={styles.projectLink} href='https://webheroschool.github.io/zubtsova-final-HTML-CSS/'>Axion</a></li>
+        <li><a className={styles.projectLink} href='https://viktoriya-zubtsova.github.io/Bali/'>Bali</a></li>
+        <li><a className={styles.projectLink} href='https://webheroschool.github.io/zubtsova-JS/'>Карты с багами оО</a></li>
+        </ul>
         </div>}
         <h2 className={styles.title}>{ isLoading ? <LinearProgress color="primary" /> : 'Мои репозитории:'}</h2>
         {!isLoading && <div>
           <ol className={styles.list}>
-            {this.state.repoPaginList.map(repo => (<li className={styles.listItem} key={repo.id}>
-              <a className={styles.projectLink} href={repo.html_url}>{repo.name}</a>
-              <p className={styles.text}>{repo.description} ({repo.language})</p>
-              <span className={styles.text}>обновлён: {new Date(repo.updated_at).toLocaleString()}</span>
-            </li>))}
+          {this.state.repoPaginList.map(repo => (<li className={styles.listItem} key={repo.id}>
+          <a className={styles.projectLink} href={repo.html_url}>{repo.name}</a>
+          <p className={styles.text}>{repo.description} ({repo.language})</p>
+          <span className={styles.text}>обновлён: {new Date(repo.updated_at).toLocaleString()}</span>
+          </li>))}
           </ol>
           <div className={styles.pagination}>
-            <Pagination
-              count={this.state.paginCount}
-              variant="outlined"
-              color="primary"
-              onChange={this.changePagin.bind(this)} />
+          <Pagination
+          count={this.state.paginCount}
+          variant="outlined"
+          color="primary"
+          onChange={this.changePagin.bind(this)} />
           </div>
-        </div>}
-      </div>
-    );
-  }
-  }
-}
+          </div>}
+          </div>
+          );
+        }
+      }
+    }
 
-export default About;
+    export default About;
